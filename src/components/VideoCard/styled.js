ea9796e2e98e7266152598acb30eee1840735f01
jonasaky <luis.garcia@wizeline.com>
2021-03-05 21:51:51 -0600

    updated VideoCard style and selection function

--- src/components/VideoCard/styled.js
+++ src/components/VideoCard/styled.js
@@ -3,17 +3,31 @@ import styled from 'styled-components';
 export const VideoCard = styled.div`
     display: flex;
     flex-direction: column;
-    width: 300px;
+    width: 400px;
     height: 300px;
-    padding: 0 10px;
+    margin: 10px;
+    border-radius: 8px;
+    box-shadow: 4px 4px 8px lightgray;
+    cursor: pointer;
 `;
 
-export const VideoImg = styled.img`
-    width: 200px;
+export const VideoImgWrapper = styled.div`
+    width: 100%;
+    height: 220px;
+    overflow: hidden;
+    border-radius: 8px 8px 0 0;
+`;
+
+export const Img = styled.img`
+    width: 100%;
 `;
 
 export const VideoTitle = styled.p`
-    background-color: rgba(0, 122, 122, 0.23);
-    z-index: 1;
-    height: 180px;
+    background-color: #363e48;
+    color: #c9d1d9;
+    padding-left: 8px;
+    height: 80px;
+    margin: 0;
+    text-align: left;
+    border-radius: 0 0 8px 8px;
 `;
