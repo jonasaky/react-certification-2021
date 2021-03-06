34aacea7e533dc1f64444bd8053a34cfa2cac55b
jonasaky <luis.garcia@wizeline.com>
2021-03-05 21:52:28 -0600

    updated Home page

--- src/pages/Home/styled.js
+++ src/pages/Home/styled.js
@@ -3,19 +3,24 @@ import { Link } from 'react-router-dom';
 
 export const HomePage = styled.section`
     text-align: center;
+    margin-top: 50px;
 `;
 
 export const HomeTitle = styled.h1`
     font-size: 3rem;
     letter-spacing: -2px;
     font-weight: lighter;
+    @media (max-width: 768px) {
+        font-size: 1.5rem;
+    }
 `;
 
 export const WrapperVideos = styled.div`
     display: flex;
     flex-direction: row;
     flex-wrap: wrap;
-    padding: 0 0 0 50px;
+    /* padding: 0 0 0 50px; */
+    justify-content: center;
 `;
 
 export const MyLink = styled(Link)`
